1dea3b37c5d7f6b238e375f1448540eb286a2685
DanielF-Cardoso <danielfca122@gmail.com>
2025-07-16 16:29:29 -0300

    feat: adiciona rota GetRooms

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
-import { sql } from './db/connection.ts'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -20,6 +20,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', async () => {
   return { status: 'ok' }
 })
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('Server is running')
